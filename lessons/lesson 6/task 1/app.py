4688b547054ec37796eb7d54ce2fafac7353a708
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2023-04-27 22:29:07 +0200

    Code style fixes in task 1

--- lessons/lesson 6/task 1/app.py
+++ lessons/lesson 6/task 1/app.py
@@ -1,15 +1,17 @@
 def get_unused_values(array_1, array_2):
     res = []
 
-    for num in array_1:
+    for num in set(array_1):
         if num not in array_2:
             res.append(num)
 
     return res
 
+
 def read_list_numbers(mess):
     data = input(mess)
     return [int(num) for num in data.split(" ")]
+
 
 # list_1 = [1, 2, 3, 4, 5, 6]
 # list_2 = [4, 5, 6, 7, 8]
